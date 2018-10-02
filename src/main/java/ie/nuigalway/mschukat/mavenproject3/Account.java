aba749304219a5858b153bd592c6a2b6d4a23d3a
Michael Schukat <michael.schukat@nuigalway.ie>
2018-10-02 10:54:32 +0100

    Version 1

---
+++ src/main/java/ie/nuigalway/mschukat/mavenproject3/Account.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ie.nuigalway.mschukat.mavenproject3;
+
+/**
+ *
+ * @author user
+ */
+public class Account {
+    String name;
+    int balance;
+    String address;
+    
+    public Account(String name, int bal)
+    {
+        // CT417Class ct417 = new CT417Class();
+        this.name = name;
+        this.balance = bal;
+        this.address = "Mayo";
+    }
+    
+    public String getName()
+    {
+        return this.name;
+    }
+    
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    
+    public void setBalance(int balance)
+    {
+        this.balance = balance;
+    }    
+}
---
+++ src/test/java/AccountTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import ie.nuigalway.mschukat.mavenproject3.Account;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author user
+ */
+public class AccountTest {
+    
+    public AccountTest() {
+    }
+
+    @Test
+    public void evaluatesGetName() {
+    Account acc = new Account("Michael", 100);
+    assertEquals(acc.getName(), "Michael");
+  } 
+}
